fe25e4956aafc5bb95618bb976178fd82a32414c
Simon <kartsims@gmail.com>
2017-01-25 13:47:20 +0100

    cleanup

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ CROP_WINDOW = 0.0, 0.0, 1.0, 1.0
 # configure the font used for text display
 FONT_FILE = "dizzyedge.otf"
 FONT_COLOR = 255, 255, 255
+FONT_SIZE = 20
 
 # time in secs before taking the picture
-PHOTO_DELAY = 0
-# PHOTO_DELAY = 3
+PHOTO_DELAY = 3
 
 # duration in secs to display the new picture
 PHOTO_DISPLAY_DURATION = 7
@@ -25,3 +25,17 @@ PRINT_DELAY = 3
 
 # directory where images are stored
 PHOTOS_DIR = "./photos/"
+
+# keyboard inputs
+import pygame
+KEY_PHOTO = pygame.K_SPACE
+KEY_PRINT = pygame.K_RETURN
+
+# GPIO inputs
+ENABLE_GPIO = True
+GPIO_PHOTO = 18
+GPIO_PRINT = 16
+
+# text translations
+TEXT_PRINT_CONFIRM = "Print this photo ?"
+TEXT_PRINT_WAIT = "Printing ! Please wait..."
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# access libraries from lib/
 import sys
+import os
+sys.path.insert(1, os.path.join(sys.path[0], 'lib'))
+
+# import required libraries
 import math
 import pygame
 import time
@@ -19,11 +24,11 @@ yuv = bytearray(SCREEN_RESOLUTION[0] * SCREEN_RESOLUTION[1] * 3 / 2)
 pygame.init()
 pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-# disable full screen (might be useful when accessed through SSH)
+# disable full screen (might be useful when accessed through "ssh -X")
 # screen = pygame.display.set_mode(SCREEN_RESOLUTION)
 
 # init font file
-font = pygame.font.Font("./fonts/" + FONT_FILE, 46)
+font = pygame.font.Font("./fonts/" + FONT_FILE, FONT_SIZE)
 
 # init camera
 camera = picamera.PiCamera()
@@ -31,6 +36,12 @@ atexit.register(camera.close)
 camera.resolution = SCREEN_RESOLUTION
 camera.crop = CROP_WINDOW
 
+# init GPIO pins
+if ENABLE_GPIO:
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_PHOTO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(GPIO_PRINT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def displayText(text):
@@ -77,10 +88,12 @@ def getLastPhoto():
 
 def showLastPhoto():
     lastPhoto = getLastPhoto()
+    if not lastPhoto:
+        return
     fullSize = pygame.image.load(lastPhoto)
     img = pygame.transform.scale(fullSize, SCREEN_RESOLUTION)
     displayImage(img)
-    displayText("Print this photo ?")
+    displayText(TEXT_PRINT_CONFIRM)
     pygame.display.update()
     pygame.event.clear()
     buttonTime = time.clock()
@@ -88,20 +101,29 @@ def showLastPhoto():
         timeFromButton = int(math.ceil(time.clock() - buttonTime))
         if timeFromButton > PHOTO_DISPLAY_DURATION:
             return
+        # listen for keyboard inputs
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
+                if event.key == KEY_PHOTO:
+                    return
+                elif event.key == KEY_PRINT:
                     printLastPhoto()
                     return
-                elif event.key == pygame.K_SPACE:
-                    return
+        # listen for GPIO inputs
+        if ENABLE_GPIO:
+            if GPIO.input(GPIO_PHOTO) == False:
+                time.sleep(.3)
+                return
+            if GPIO.input(GPIO_PRINT) == False:
+                printLastPhoto()
+                return
 
 def printLastPhoto():
     lastPhoto = getLastPhoto()
     fullSize = pygame.image.load(lastPhoto)
     img = pygame.transform.scale(fullSize, SCREEN_RESOLUTION)
     displayImage(img)
-    displayText("Printing ! Please wait...")
+    displayText(TEXT_PRINT_WAIT)
     pygame.display.update()
     # TODO start printing
     time.sleep(PRINT_DELAY)
@@ -121,10 +143,18 @@ def startCountdown():
             lastPhoto = takePicture()
             showLastPhoto()
             return
+
+        # listen for keyboard inputs
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == KEY_PHOTO:
                     return
+        # listen for GPIO inputs
+        if ENABLE_GPIO:
+            if GPIO.input(GPIO_PHOTO) == False:
+                time.sleep(.3)
+                return
+
 
 
 # main loop
@@ -135,10 +165,18 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
-            elif event.key == pygame.K_SPACE:
+            elif event.key == KEY_PHOTO:
                 startCountdown()
-            elif event.key == pygame.K_RETURN:
+            elif event.key == KEY_PRINT:
                 showLastPhoto()
+
+    # listen for GPIO inputs
+    if ENABLE_GPIO:
+        if GPIO.input(GPIO_PHOTO) == False:
+            time.sleep(.3)
+            startCountdown()
+        if GPIO.input(GPIO_PRINT) == False:
+            showLastPhoto()
 
     # show camera preview
     showCamPreview()
